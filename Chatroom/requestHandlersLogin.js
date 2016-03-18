c3aecbf8893c515ed1258bd080e003f482784f7a
JanetRahn <JanetRahn@msn.com>
2016-03-18 10:55:28 +0100

    HTML "name" hinzugefügt und in requestHandlers dementsprechend abgeändert.

--- Chatroom/requestHandlersLogin.js
+++ Chatroom/requestHandlersLogin.js
@@ -8,29 +8,29 @@ var fs = require('fs');
  */
 
 function start(response, postData) {
-  console.log("Request handler 'start' was called."); 
-
- fs.readFile('./view/html/login.html', function(err, html){
-	if(err){
-		throw err;
-	}
-	response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(html);
-    response.end();
- }); 
+	console.log("Request handler 'start' was called.");
+	fs.readFile('./view/html/login.html', function(err, html) {
+		if (err) {
+			throw err;
+		}
+		response.writeHead(200, {
+			"Content-Type" : "text/html",
+		});
+		response.write(html);
+		response.end();
+	});
 }
 /**
  * Funktion wird durch Klick auf den Login-Button aufegruden
  * @param response HTML-Seite wird an den Client übergeben
  * @param postData
  */
-//Nach dem Clicken auf den Button
-function joinChat(response, postData) { 
-	  response.writeHead(200, {"Content-Type": "text/plain"}); 
-	  response.write("You've sent the text: "+ querystring.parse(postData).text);
-	  response.end();
-	}
+// Nach dem Clicken auf den Button
+function joinChat(response, postData) {
+	response.writeHead(200, {"Content-Type" : "text/plain"});
+	response.write("You've sent the text: " + querystring.parse(postData).username);
+	response.end();
+}
 
 exports.start = start;
 exports.joinChat = joinChat;
-
