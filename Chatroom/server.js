ee808b02aaf0c9c6142dd1ef4e8bf11949756c8c
JanetRahn <JanetRahn@msn.com>
2016-03-22 17:15:44 +0100

    Funktionen:  - Zeit vor Name  - Meldung Beitreten/Verlassen  - Willkommensnachricht nur an Client

--- Chatroom/server.js
+++ Chatroom/server.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var clients = {};
 
 app.use(express.static(__dirname + '/public'));
-                                                                           
+
 app.post('/joinChat', function(req, res) {
 	res.sendFile(__dirname + '/public/chat.html');
 });
@@ -20,17 +20,38 @@ http.listen(8888, function() {
 });
 
 io.sockets.on('connection', function(socket) {
-
-	socket.on('infoUser', function(username){
-		socket.emit('servermessage', 'Welcome to the Chat, ' + username);
-		console.log('infoUser: ' + username);
-		clients[socket.id] = username; 
-		console.log('Socket id: ' + socket.id);
-	});
+	var items = Object.keys(clients);
 	
-	socket.on('chatmessage', function(message){
-		console.log('Socket id: ' + socket.id + '  Name: ' + clients[socket.id] + '   message: ' + message );
-		io.emit('message_to_client', {message: message, username: clients[socket.id]});
+	
+	socket.on('infoUser', function(username) {
+		clients[socket.id] = username;
+		items = Object.keys(clients);
+		socket.emit('servermessage', 'Welcome to the Chat, ' + username);
+
+		for (var i = 0; i < items.length; i++) {
+			if (items[i] !== socket.id) {
+				io.to(items[i]).emit('servermessage', username + ' betritt den Chat.');
+			}
+		}
 	});
+
+	socket.on('chatmessage', function(message) {
+		console.log('Socket id: ' + socket.id + '  Name: ' + clients[socket.id]	+ '   message: ' + message);
+		io.emit('message_to_clients', {
+			zeit : new Date(),
+			message : message,
+			username : clients[socket.id]
+		});
+	});
+
+	socket.on('disconnect', function(){
+		for(var i = 0; i < items.length; i++){
+			if(socket.id === items[i]){
+				io.emit('servermessage', clients[socket.id] + ' verlässt den Chat.'); 
+			
+			}
+		}
+	  });
 });
+
 
