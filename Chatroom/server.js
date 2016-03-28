6fbb76e5042d346bebe6fe284f6c3473bfbbd529
JanetRahn <JanetRahn@msn.com>
2016-03-28 12:59:19 +0200

    FINAL VERSION: private Nachricht an sich selbst wird abgefangen Liste von Username bei Wer ist Online ist anders formattiert. Kommentare hinzugefügt + Alerts entfernt
    TODO, falls Zeit:
    ordentliche Textformation

--- Chatroom/server.js
+++ Chatroom/server.js
@@ -5,36 +5,40 @@ var io = require('socket.io').listen(http);
 var fs = require('fs');
 var bodyParser = require('body-parser');
 
-//Map für Clients und die dazugehörige Socketid
+// Map für Clients und die dazugehörige Socketid
 var clients = {};
-//geparstes Array Objekt von Clients für den Zugriff und Iteration
-var socketIDs=[];
+// geparstes Array Objekt von Clients für den Zugriff und Iteration
+var socketIDs = [];
 
-//für den statischen Zugriff auf HTML/CSS Seien
+// für den statischen Zugriff auf HTML/CSS Seien
 app.use(express.static(__dirname + '/public'));
-//parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
 
 /**
- * Requesthandler als Middleware. Username wird aus der Request entnommen und überprüft,
- * ob der Name bereits in Verwendung ist. Falls der Name in Verwendung ist, wird ein Redirect durchgefüht und als Hashparamter ein Error übergeben,
- * der auf der Client-Seite abgefangen wird und die entsprechende Meldung dafür ausgibt
+ * Requesthandler als Middleware. Username wird aus der Request entnommen und
+ * überprüft, ob der Name bereits in Verwendung ist. Falls der Name in
+ * Verwendung ist, wird ein Redirect durchgefüht und als Hashparamter ein Error
+ * übergeben, der auf der Client-Seite abgefangen wird und die entsprechende
+ * Meldung dafür ausgibt
  */
 app.post('/joinChat', function(req, res) {
-	//Überprüfung, ob Username bereits verwendet wird
-	var ok = true; 
-	if(socketIDs.length !== 0){
-		for(var i = 0; i < socketIDs.length; i++){
-			if(req.body.username === clients[socketIDs[i]]){
+	// Überprüfung, ob Username bereits verwendet wird
+	var ok = true;
+	if (socketIDs.length !== 0) {
+		for (var i = 0; i < socketIDs.length; i++) {
+			if (req.body.username === clients[socketIDs[i]]) {
 				ok = false;
 				res.redirect("index.html" + "#error");
 			}
 		}
 	}
-		if(ok){
-			res.sendFile(__dirname + '/public/chat.html');
-			ok = true; 
-		}
+	if (ok) {
+		res.sendFile(__dirname + '/public/chat.html');
+		ok = true;
+	}
 });
 
 /**
@@ -47,13 +51,23 @@ http.listen(8888, function() {
 	console.log("");
 });
 
-function checkUsername(usernameTo){
+/**
+ * Überprüft den eingegebenen Username, ob dieser in der Liste vorhanden ist.
+ * eine private Nachricht an sich, kann nicht geschrieben werden.
+ * @param usernameTo Eingegebener Username
+ * @param socket Aktueller Socket
+ * @returns {Boolean} Gibt zurück, ob Username in der Liste vorhanden ist.
+ */
+function checkUsername(usernameTo, socket) {
 	var inList = false;
-	for(var i = 0; i < socketIDs.length; i++){
-		if(usernameTo === clients[socketIDs[i]]){
-			inList = true; 
+	if (usernameTo !== clients[socket.id]) {
+		for (var i = 0; i < socketIDs.length; i++) {
+			if (usernameTo === clients[socketIDs[i]]) {
+				inList = true;
+			}
 		}
 	}
+
 	return inList;
 }
 
@@ -61,12 +75,15 @@ function checkUsername(usernameTo){
  * Websocket: Connection für den Kommunikationsaufbau zwischen Client und Server
  */
 io.sockets.on('connection', function(socket) {
-	socketIDs = Object.keys(clients);	
-	
+	socketIDs = Object.keys(clients);
+
 	/**
-	 * infoUser: Bekommt vom Client den Username, damit dieser mit seiner Socketid in die Map gespeichert werden kann.
-	 * Map(clients) wird in ein Array für den Zugriff geparst (socketIds - siehe oben). Eine Willkommensnachricht wird nur an diesen einen Client über seine Socketid geschickt.
-	 * Zusätzlich werden alle anderen Clients von seinem Beitritt benachrichtig
+	 * infoUser: Bekommt vom Client den Username, damit dieser mit seiner
+	 * Socketid in die Map gespeichert werden kann. Map(clients) wird in ein
+	 * Array für den Zugriff geparst (socketIds - siehe oben). Eine
+	 * Willkommensnachricht wird nur an diesen einen Client über seine Socketid
+	 * geschickt. Zusätzlich werden alle anderen Clients von seinem Beitritt
+	 * benachrichtig
 	 */
 	socket.on('infoUser', function(username) {
 		clients[socket.id] = username;
@@ -75,70 +92,84 @@ io.sockets.on('connection', function(socket) {
 
 		for (var i = 0; i < socketIDs.length; i++) {
 			if (socketIDs[i] !== socket.id) {
-				io.to(socketIDs[i]).emit('servermessage', username + ' betritt den Chat.');
+				io.to(socketIDs[i]).emit('servermessage',
+						username + ' betritt den Chat.');
 			}
 		}
 	});
-	
+
 	/**
-	 * chatmessage: überprüft Nachrichten, ob es ein Kommando ist oder eine normale Chatnachricht an alle.
-	 * Wird als Kommando "/w username" eingegeben so erhält der User in seinem Textfeld ein Feedback-Text, an wen die Nachricht geht.
-	 * Danach kann dieser seine Nachricht direkt hinter diesem Text eingeben und abschicken. Überprüft wird hierbei der String ("Private Nachricht an"
-	 * erhält der Text diesen String + den Username, so wird eine private Nachricht an diesen Usernamen geschickt (funktioniert also auch ohne Kommando)
-	 * Ist der Username nicht vorhanden erhält der User eine Nachricht mit "User nicht vorhanden"
-	 *  
+	 * chatmessage: überprüft Nachrichten, ob es ein Kommando ist oder eine
+	 * normale Chatnachricht an alle. Wird als Kommando "/w username" eingegeben
+	 * so erhält der User in seinem Textfeld ein Feedback-Text, an wen die
+	 * Nachricht geht. Danach kann dieser seine Nachricht direkt hinter diesem
+	 * Text eingeben und abschicken. Überprüft wird hierbei der String ("Private
+	 * Nachricht an" erhält der Text diesen String + den Username, so wird eine
+	 * private Nachricht an diesen Usernamen geschickt (funktioniert also auch
+	 * ohne Kommando) Ist der Username nicht vorhanden erhält der User eine
+	 * Nachricht mit "User nicht vorhanden"
+	 * 
 	 */
 	socket.on('chatmessage', function(message) {
 		var usernameTo = "";
-		if(message.startsWith("/")){
+		if (message.startsWith("/")) {
 			var command = message.substring(1, 2);
 			var available = false;
-			switch (command){
+			switch (command) {
 			case "w":
 				usernameTo = message.substr(3);
-				//Leerzeichen löschen 
+				// Leerzeichen löschen
 				usernameTo = usernameTo.trim(" ");
-				available = checkUsername(usernameTo);
+
+				available = checkUsername(usernameTo, socket);
 
 				socket.emit('UserAvailableForPrivateMessage', {
 					available : available,
 					usernameTo : usernameTo
 				});
-				available = false; 
+				available = false;
 				break;
-			case "o": 
-				console.log("online");
+			case "o":
+				var userOnline = "";
+				for (var j = 0; j < socketIDs.length; j++) {
+					userOnline += clients[socketIDs[j]] + " ";
+				}
+				socket.emit('servermessage',
+						"<span class = 'onlineList'> Online: " + userOnline
+								+ "</span>");
 				break;
 			}
-		}else{
-			if(message.startsWith("Private Message an")){
+		} else {
+			if (message.startsWith("Private Message an")) {
 				message = message.replace("Private Message an ", "");
-				usernameTo = message.substring(0, message.lastIndexOf(":")); 
-				if(checkUsername(usernameTo)=== true){
+				usernameTo = message.substring(0, message.lastIndexOf(":"));
+				if (checkUsername(usernameTo, socket) === true) {
 					var tmp = usernameTo + ":";
 					message = message.replace(tmp, "").trim(" ");
-					message = "<span class='privateMessage'>" + message + "<span>";
-					for(var j = 0; j < socketIDs.length; j++){
-						if(usernameTo === clients[socketIDs[j]] || socket.id === clients[socketIDs[j]]){
-							io.to(socketIDs[j]).emit('message_to_client',{
-									zeit : new Date(),
-									message : message,
-									username : clients[socket.id],
+					message = "<span class='privateMessage'>" + message
+							+ "<span>";
+					for (var j = 0; j < socketIDs.length; j++) {
+						if (usernameTo === clients[socketIDs[j]]
+								|| socket.id === clients[socketIDs[j]]) {
+							io.to(socketIDs[j]).emit('message_to_client', {
+								zeit : new Date(),
+								message : message,
+								username : clients[socket.id],
 							});
-							socket.emit('message_to_client',{
-							zeit : new Date(),
-							message : message,
-							username : clients[socket.id],
+							socket.emit('message_to_client', {
+								zeit : new Date(),
+								message : message,
+								username : clients[socket.id],
 							});
 						}
 					}
-				}else{
+				} else {
 					socket.emit('UserAvailableForPrivateMessage', {
 						available : false,
 						usernameTo : usernameTo
 					});
-				}				
-			}else{
+				}
+			} else {
 				io.emit('message_to_client', {
 					zeit : new Date(),
 					message : message,
@@ -149,19 +180,18 @@ io.sockets.on('connection', function(socket) {
 	});
 
 	/**
-	 * User, der sich ausloggt (Fenster schließt) wird aus der Liste entnommen 
-	 * und es wird eine Broadcast-Nachricht an alle gesendet und informariert, 
+	 * User, der sich ausloggt (Fenster schließt) wird aus der Liste entnommen
+	 * und es wird eine Broadcast-Nachricht an alle gesendet und informariert,
 	 * dass dieser Benutzer den Chat verlassen hat
 	 */
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function() {
 		for (var i = 0; i < socketIDs.length; i++) {
 			if (socket.id === socketIDs[i]) {
-				io.emit('servermessage', clients[socket.id] + ' verlässt den Chat.');
+				io.emit('servermessage', clients[socket.id]
+						+ ' verlässt den Chat.');
 				delete clients[socket.id];
 				socketIDs = Object.keys(clients);
 			}
 		}
-	  });
+	});
 });
-
-
